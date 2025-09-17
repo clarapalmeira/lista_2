6d95961393914d2a4f8598f26a87fac3ed2ed79f
clarapalmeira <clara.palmeira12@aluno.ifce.edu.br>
2025-09-17 09:06:53 -0300

    Create questão_2.c

---
+++ questão_2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int dia;
+
+   do { printf("Digite um número correspondente ao dia da semana (1 a 7 ou 0 para sair): ");
+    scanf("%d", &dia);
+
+    while (dia != 0) {
+        switch (dia) {
+            case 1:
+                printf("Domingo\n");
+                break;
+            case 2:
+                printf("Segunda\n");
+                break;
+            case 3:
+                printf("Terça\n");
+                break;
+            case 4:
+                printf("Quarta\n");
+                break;
+            case 5:
+                printf("Quinta\n");
+                break;
+            case 6:
+                printf("Sexta\n");
+                break;
+            case 7:
+                printf("Sábado\n");
+                break;
+            case 0:
+                 printf("Número de dia não válido. Encerrando o programa.\n");
+                 break;
+            default:
+                printf("Número de dia não válido.\n");
+                break;
+        }
+
+        printf("\nDigite um número correspondente ao dia da semana (1 a 7 ou 0 para sair): ");
+        scanf("%d", &dia);
+    }
+
+   } while (dia != 0);
+
+    return 0;
+}
